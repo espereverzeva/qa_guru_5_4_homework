d2722033bc11f4f69dbaffbc2e2a556c98c20e36
espereverzeva <katya_novikova_1990_29@mail.ru>
2021-04-02 23:45:46 +0700

    Add

---
+++ src/test/java/pageobjects/scenarios/StudentRegistrationFormTests.java
@@ -0,0 +1,15 @@
+package pageobjects.scenarios;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StudentRegistrationFormTests {
+    StudentRegistrationFormPage studentRegistrationFormPage = new StudentRegistrationFormPage();
+
+    @Test
+    void successfulRegistrationTest() {
+        studentRegistrationFormPage.openPage();
+        studentRegistrationFormPage.fillPage();
+        studentRegistrationFormPage.checkData();
+    }
+}
